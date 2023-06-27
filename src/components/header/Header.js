1089951c149a488f28a382fd1d4f42fa116fa112
Kirbee Parsons <kirbeeleanne@gmail.com>
2023-06-27 14:35:52 -0700

    add nvm, tweaks

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const Header = () => <header>Kirbee Parsons | Web Developer</header>;
+const Header = () => (
+  <header>
+    <h1>Kirbee Parsons | Web Developer</h1>
+  </header>
+);
 
 export { Header };
