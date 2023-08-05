fc1e19895fa5225b53d2dc795f1dab7cd49638f9
Kirbee Parsons <kirbeeleanne@gmail.com>
2023-08-05 12:51:45 -0700

    add some basic heading styles, play around with css custom properties

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { header, heading, subheading } from './Header.module.scss';
 
 const Header = () => (
-  <header>
-    <h1>Kirbee Parsons | Web Developer</h1>
+  <header className={header}>
+    <h1 className={heading}>Kirbee Parsons</h1>
+    <h2 className={subheading}>web developer</h2>
   </header>
 );
 
