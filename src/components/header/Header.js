86e4ad6727fb4a7b6d838c3daac050300bd41dc9
Kirbee Parsons <kirbeeleanne@gmail.com>
2023-08-15 20:52:57 -0700

    simple header complete

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { header, heading, subheading } from './Header.module.scss';
+import { Link } from 'gatsby';
+import { header, heading, links } from './Header.module.scss';
 
 const Header = () => (
   <header className={header}>
     <h1 className={heading}>Kirbee Parsons</h1>
-    <h2 className={subheading}>web developer</h2>
+    <ul className={links}>
+      <Link to="/about">About</Link>
+      <Link to="/blog">Blog</Link>
+      <Link to="/resources">Resources</Link>
+    </ul>
   </header>
 );
 
